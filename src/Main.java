cd292a999bc475900447c1645a442757e4ada1d2
Nikos Katrakoulis <224615343+NikosKatrakoulis@users.noreply.github.com>
2026-03-17 16:55:01 +0200

    Add menu and scanner

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,28 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner();
+        String welcome_message = """
+              =============== CLI CALCULATOR ===============
+              
+                      **Welcome to my CLI calculator**
+              
+                      Give the numbers and the operators
+                      and the calculator will show the 
+                                results.""";
+
+        System.out.println(welcome_message);
+
+        System.out.println("=== MENU ===");
+        System.out.println("1. Addition");
+        System.out.println("2. Subtraction");
+        System.out.println("3. Multiply");
+        System.out.println("4. Division");
+        System.out.println("5. Exit");
+        System.out.println("Choose one:");
+
+        System.out.println("Type the first number. Type \"exit\" anytime you want to quit.");
+        firstNumber =
+    }
 }
